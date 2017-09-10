f6217553e914b9dea194d655c5e9bd382555da42
Phanoix <phanoix@gmail.com>
2017-09-10 18:40:51 -0400

    adding possibly needed packages

--- gql_srv.go
+++ gql_srv.go
@@ -1,10 +1,12 @@
 package graphql_usr_srv
 
-import { 
-		"github.com/neelance/graphql-go"
-		"fmt"
-}
+import "github.com/neelance/graphql-go"
+import "fmt"
+import "net/http"
+import "regexp"
 
 func main() {
     fmt.Printf("Server starting...\n")
 }
+
+
--- gql_srv.go_test.go
+++ gql_srv.go_test.go
@@ -3,7 +3,6 @@ package graphql_usr_srv
 import "testing"
 
 func TestTypesAvailable(t *testing.T){
-	types := {
-			"user"
-	}
+	var check_types [1]string
+	check_types[0] = "User"
 }
