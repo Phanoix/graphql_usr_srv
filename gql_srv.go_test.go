f0814683f2193e972eabbe8192058cb8cccb8b55
Phanoix <phanoix@gmail.com>
2017-09-11 16:21:39 -0400

    session type existance test

--- gql_srv.go_test.go
+++ gql_srv.go_test.go
@@ -3,9 +3,7 @@ package main
 import "testing"
 
 func TestTypesAvailable(t *testing.T){
-	var check_types [1]string
-	check_types[0] = "User"
-
+	// test user type existence
 	testUser := user{
 		ID:			"1",
 		Username:	"testuser",
@@ -14,5 +12,17 @@ func TestTypesAvailable(t *testing.T){
 
 	if testUser.Username != "testuser" {
 		panic("failed user type testuser");
+	}
+
+	// test session type existence
+	testSession := session{
+		ID:			"634326846daf1",
+		UserID:		"1",
+		Username:	"testuser",
+		Expires:	15488632,
+	}
+
+	if testSession.ID != "634326846daf1" {
+		panic("failed session type testuser");
 	}
 }
